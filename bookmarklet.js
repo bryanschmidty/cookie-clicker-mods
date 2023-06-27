853c5c8aae5c050eb3a30abde2a41af957bcfe34
Bryan Schmidt <bryan@fashionphile.com>
2023-06-27 14:14:25 -0600

    bookmarklet (broken)

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+javascript:(function() {
+    var script = document.createElement('script');
+    script.src = 'https://thingproxy.freeboard.io/fetch/https://raw.githubusercontent.com/bryanschmidty/cookie-clicker-mods/master/epicCookie.js';
+    document.body.appendChild(script);
+})();
